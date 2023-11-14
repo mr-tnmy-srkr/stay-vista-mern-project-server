856ecd4ca5f292a74fda8c274403b054a792c14a
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-14 16:31:06 +0530

    updated

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const client = new MongoClient(process.env.DB_URI, {
     deprecationErrors: true,
   },
 })
-async function run() {
+/* async function run() {
   try {
     // auth related api
     app.post('/jwt', async (req, res) => {
@@ -104,7 +104,85 @@ async function run() {
     // await client.close();
   }
 }
-run().catch(console.dir)
+run().catch(console.dir) */
+
+async function run() {
+  try {
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      try {
+        const user = req.body;
+        console.log('I need a new jwt', user);
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '365d',
+        });
+        res
+          .cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true });
+      } catch (jwtError) {
+        res.status(500).send(jwtError);
+      }
+    });
+
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true });
+        console.log('Logout successful');
+      } catch (logoutError) {
+        res.status(500).send(logoutError);
+      }
+    });
+
+    // Save or modify user email, status in DB
+    app.put('/users/:email', async (req, res) => {
+      try {
+        const email = req.params.email;
+        const user = req.body;
+        const query = { email: email };
+        const options = { upsert: true };
+        const isExist = await usersCollection.findOne(query);
+        console.log('User found?----->', isExist);
+        if (isExist) return res.send(isExist);
+        const result = await usersCollection.updateOne(
+          query,
+          {
+            $set: { ...user, timestamp: Date.now() },
+          },
+          options
+        );
+        res.send(result);
+      } catch (dbError) {
+        res.status(500).send(dbError);
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    try {
+      await client.db('admin').command({ ping: 1 });
+      console.log(
+        'Pinged your deployment. You successfully connected to MongoDB!'
+      );
+    } catch (pingError) {
+      console.error('MongoDB ping error:', pingError);
+    }
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello from StayVista Server..')
